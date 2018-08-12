b727db88e8f0bf76568f4bea16cb4beb571d5727
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-12 09:30:34 +0200

    husky

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,85 +1,85 @@
-const pkg = require('./package')
+const pkg = require('./package');
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
+    /*
    ** Headers of the page
    */
-  head: {
-    title: pkg.name,
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: pkg.description
-      }
-    ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
-  },
+    head: {
+        title: pkg.name,
+        meta: [{
+            charset: 'utf-8'
+        },
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1'
+        },
+        {
+            hid: 'description',
+            name: 'description',
+            content: pkg.description
+        }
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        }]
+    },
 
-  /*
+    /*
    ** Customize the progress-bar color
    */
-  loading: {
-    color: '#FFFFFF'
-  },
+    loading: {
+        color: '#FFFFFF'
+    },
 
-  /*
+    /*
    ** Global CSS
    */
-  css: [],
+    css: [],
 
-  /*
+    /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
+    plugins: [
 
-  ],
+    ],
 
-  /*
+    /*
    ** Nuxt.js modules
    */
-  modules: [
-    ['storyblok-nuxt', {
-      accessToken: 'rnWt2D5baTkDi6DgLKNpAAtt',
-      cacheProvider: 'memory'
-    }]
-  ],
-  /*
+    modules: [
+        ['storyblok-nuxt', {
+            accessToken: 'rnWt2D5baTkDi6DgLKNpAAtt',
+            cacheProvider: 'memory'
+        }]
+    ],
+    /*
    ** Axios module configuration
    */
-  axios: {
+    axios: {
     // See https://github.com/nuxt-community/axios-module#options
-  },
+    },
 
-  /*
+    /*
    ** Build configuration
    */
-  build: {
+    build: {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+        extend(config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                });
+            }
+        }
     }
-  }
-}
+};
